0dcdf5123321c9dc96c51ee68e3e554cbaa5ff73
bioked <brkanovic@pm.me>
2025-10-08 23:36:40 +0200

    chore: clean up utils

--- parse_json.py
+++ parse_json.py
@@ -1,15 +1,11 @@
 import json
 from pathlib import Path
 
-# Point to your JSON file
 file_path = Path("sample.json")
 
-# Read it as plain text
 with file_path.open("r", encoding="utf-8") as f:
 	raw_text = f.read()
 
-# Parse the JSON string into Python objects
 data = json.loads(raw_text)
 
-#Pretty-print the data as JSON again 
 print(json.dumps(data, indent=4))
